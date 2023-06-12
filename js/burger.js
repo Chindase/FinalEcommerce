1f0cf40e260873d825a4950d1862c72b0a61b09c
Chindase <schinda96@gmail.com>
2023-06-12 02:59:41 +0000

    first commit

---
+++ js/burger.js
@@ -0,0 +1,11 @@
+// This code closes the burger menu
+const closeBurger_menu = () => {
+    document.getElementById("overlay_menu").style.display = "none";
+};
+
+// This code opens the burger menu
+const openBurger_menu = () => {
+    document.getElementById("overlay_menu").style.display = "block";
+}
+
+
